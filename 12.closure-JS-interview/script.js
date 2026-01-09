c63dd29ba231273d37dd0682d960834e676e73ce
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-01-09 19:07:59 +0530

    closures interview experience

---
+++ 12.closure-JS-interview/script.js
@@ -0,0 +1,17 @@
+/*  1.what is the closure in js?
+A closure is the combination of function with its lexical environment
+*/
+
+// example
+/*
+function x(){
+    var a = 10;
+    function y(){
+        console.log(a)
+    }
+    return y();
+}
+
+x();
+*/
+
